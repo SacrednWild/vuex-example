14dd6dd06a5cce9aff24fc9d59e216fa5b98e030
Michele Rosito <githubmichelerosito@gmail.com>
2020-03-28 10:35:11 +0100

    Fixed Vuex Store reference to perform the Vue App

--- src/store/index.js
+++ src/store/index.js
@@ -4,22 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state = { numbers: [1,2,3] },    // Property to share within the application
-  mutations = {                    // Functions that mutate the store's state directly
+  state: { numbers: [1,2,3] },    // Property to share within the application
+  mutations: {                    // Functions that mutate the store's state directly
     ADD_NUMBER( state, payload ) {
       state.numbers.push( payload );
     }
   },
-  actions = {                      // Functions that call mutations and perform all asynchronous calls 
+  actions: {                      // Functions that call mutations and perform all asynchronous calls 
     addNumber( context, number ) { // prior to committing to mutations.
       context.commit( "ADD_NUMBER", number );
     }
   },
-  getters = {                      // Functions are like COMPUTED-PROPIERTIES for Vue components:
+  getters: {                      // Functions are like COMPUTED-PROPIERTIES for Vue components:
     getNumbers( state ) {          // perform calculation to store state before having that info 
       return state.numbers;        // to components
     }
-  },  
+  },
   modules: {
   }
 })
