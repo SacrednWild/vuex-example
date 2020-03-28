ecff1aa8fb715f444a8b1985a6433fee10856978
Michele Rosito <githubmichelerosito@gmail.com>
2020-03-28 09:54:40 +0100

    Set up the Vuex Store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
 const store = new Vuex.Store( {
-  state,
-  mutations,
-  actions,
-  getters
+  state = { numbers: [1,2,3] },    // Property to share within the application
+  mutations = {                    // Functions that mutate the store's state directly
+    ADD_NUMBER( state, payload ) {
+      state.numbers.push( payload );
+    }
+  },
+  actions = {                      // Functions that call mutations and perform all asynchronous calls 
+    addNumber( context, number ) { // prior to committing to mutations.
+      context.commit( "ADD_NUMBER", number );
+    }
+  },
+  getters = {                      // Functions are like COMPUTED-PROPIERTIES for Vue components:
+    getNumbers( state ) {          // perform calculation to store state before having that info 
+      return state.numbers;        // to components
+    }
+  }
  });
