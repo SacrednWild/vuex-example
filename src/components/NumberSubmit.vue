cb4015f4da4e18a2a3d9a6a6729bb7fbc5e8f527
Michele Rosito <githubmichelerosito@gmail.com>
2020-03-28 10:17:58 +0100

    Added component who mutate Vuex Store's state

---
+++ src/components/NumberSubmit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input v-model.number="numberInput" type="number" />
+    <button v-on:click="addNumber( numberInput )"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NumberSubmit",
+  data() {
+    return { numberInput: 0 };
+  },
+  methods: {
+    addNumber( numberInput ) {
+      this.$store.dispatch( "addNumber", Number( numberInput ) );
+    }
+  }
+};
+</script>
